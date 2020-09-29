823e0bfa5885af73f2a18082b8566c9107683999
Chris Yang <cyang@deltalearns.ca>
2020-09-28 17:20:38 -0700

    Create task3.py

---
+++ task3.py
@@ -0,0 +1,17 @@
+#! python3
+"""
+Ask the user to enter a number. 
+Tell them if the number is a positive integer
+(2 points) 
+
+inputs:
+a number of any type
+
+outputs:
+xx is a positive integer.
+xx is not a positive integer
+
+example:
+Enter a number: -3
+-3 is not a positive integer
+"""
