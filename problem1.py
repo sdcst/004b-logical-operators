adb2d95921eeae253412c034d886e7ccafd2968b
Chris Yang <cyang@deltalearns.ca>
2020-09-29 09:34:52 -0700

    Create problem1.py

---
+++ problem1.py
@@ -0,0 +1,21 @@
+"""
+Problem 1
+Ask the user to enter a number.
+The number is considered "frue" if it is
+divisible by 6, but not divisible by 8.
+State whether the number is "frue" 
+(2 marks)
+
+Inputs:
+a number
+
+Outputs:
+xx is frue
+xx is not frue
+
+example:
+Enter a number: 48
+48 is frue.
+"""
+
+#! python3
