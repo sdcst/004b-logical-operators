281417d0fea42f2c28748943fd4ddc408789ea38
Chris Yang <cyang@deltalearns.ca>
2020-09-29 09:38:11 -0700

    Create problem2.py

---
+++ problem2.py
@@ -0,0 +1,25 @@
+#! python3
+
+"""
+Problem 2
+Factors are positive integers that divide evenly into another integer.
+The user will enter in two numbers. Determine if the smaller is a factor of the larger
+(2 marks)
+
+inputs:
+an integer
+an integer
+
+outputs:
+xx is a factor of yy
+xx is not a factor of yy
+
+examples:
+Enter a number: 10
+Enter another number: 2
+2 is a factor of 10
+
+Enter a number: 4
+Enter another number: 25
+4 is not a factor of 25
+"""
