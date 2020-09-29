ef5ea3cd6a10c542a674f6dc746791da77cc6523
Chris Yang <cyang@deltalearns.ca>
2020-09-28 17:25:19 -0700

    Create task4.py

---
+++ task4.py
@@ -0,0 +1,22 @@
+#! python3
+"""
+Ask the user to enter a name. 
+If the name is one of the names on a list of special users, greet them by name.
+(2 points) 
+
+Inputs:
+Name (string)
+
+Outputs:
+You are not a VIP.
+Hi xxxxxx! You are a VIP!
+
+Example:
+Enter your name=>Gertrude
+Hi Gertrude! You are a VIP!
+
+Enter your name=>Gordon
+You are not a VIP.
+"""
+
+VIPNames = ("Guile","Blanka","Christine","Carol","Richard","Daniel","Chun-Li")
