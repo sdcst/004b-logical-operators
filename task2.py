c71fc6e7168b27e955281646dec7e6052d661c01
Chris Yang <cyang@deltalearns.ca>
2020-09-28 17:19:07 -0700

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,20 @@
+#! python3
+# Task2.py
+
+"""
+Ask the user to enter a number.
+Tell them if the number is both a perfect square and a perfect cube.
+(2 points) 
+
+Inputs:
+number
+
+Outputs:
+xx is both a perfect square and a perfect cube.
+xx is only a perfect square.
+xx is only a perfect cube.
+
+Example:
+Enter a number: 8
+8 is only a perfect cube.
+"""
